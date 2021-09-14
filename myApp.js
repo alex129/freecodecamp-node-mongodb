c97921fc75cd7fe5cda2e74b9a0a8dca9ddeea22
alejandro <AlejP2121>
2021-09-14 09:45:57 +0200

    connect to database

--- myApp.js
+++ myApp.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
-var mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+equire('mongoose');
 
 let Person;
 
