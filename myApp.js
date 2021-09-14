8f234ab75605e67e0219b75960a36856ef99915f
alejandro <AlejP2121>
2021-09-14 14:18:42 +0200

    fix

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 /** 1) Install & Set up mongoose */
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const personSchema = new mongoose.Schema({
   name: String,
